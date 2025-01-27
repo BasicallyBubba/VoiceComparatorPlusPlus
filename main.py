6abc8a4d72ca04c18d62aad2fc4557c61e22d4a0
BasicallyBubba <55514925+BasicallyBubba@users.noreply.github.com>
2025-01-27 15:09:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-
-
 ###########################################################################################################
-# 
-#       BU ARAC EGITIM AMACLIDIR TAM SURUMDE DEGILDIR BU NEDENLE VERILERE TAM OLARAK GUVENILMEMELIDIR 
-#
+#                                                                                                         #
+#       This is a project, not a forensics tool. Take everything with a grain of salt.                    #
+#                                                                                                         #
 ###########################################################################################################
-
-
+# Credit to MehmetYukselSekeroglu for the original code
+# This is just a localization + refactor of his code for the most part.
 
 from resemblyzer import preprocess_wav, VoiceEncoder
 import numpy as np
@@ -21,9 +18,9 @@ import time
 from colorama import *
 
 
-# RENK KODLARI VE KALIN YAZI FONTU
-kalın ="\033[1m"
-kalın_reset ="\033[0m"
+# Style colors and whatnot
+bold ="\033[1m"
+bold_reset ="\033[0m"
 green = Fore.GREEN
 blue = Fore.BLUE
 color_reset = Fore.RESET
@@ -45,16 +42,15 @@ if not os.path.exists(TEMP_PATH):
 
 ###########################################################################################################
 
-# TERMINAL UZERINDEN BILGI VERME AMACLI FONKSIYONLAR 
+# Output functions
 
 
 def GetTime():
     """
-    herhangi parametre almadan sisteme ait güncel zamanı 
-    saat:dakika:saniye olarak döndürür
+    Returns the current system time as hour:minute:second
 
     Returns:
-        str: saat:dakika:saniye
+        str: hour:minute:second
     """
     current_time = time.localtime()
     hour = current_time.tm_hour
@@ -64,35 +60,31 @@ def GetTime():
     
     return formatted_time
 
-# BASLIKLARI YAZDIRMAK ICIN 
-def TitlePrinter(mesages:str):
-    print(f"{kalın}{blue}>> [{mesages}]{kalın_reset}{color_reset}",end="\n\n")
+# Fancy heading stylization
+def TitlePrinter(messages:str):
+    print(f"{bold}{blue}>> [{messages}]{bold_reset}{color_reset}",end="\n\n")
 
     
 
-# BILGILENDIRMELER ICIN 
-def InformationPrinter(mesages:str):
-    
-    print(f"{kalın}{blue}[{GetTime()}]{kalın}[INFO]: {green}{mesages} {color_reset}{kalın_reset}")
+# Fancy information stylization
+def InformationPrinter(messages:str):
+    print(f"{bold}{blue}[{GetTime()}]{bold}[INFO]: {green}{messages} {color_reset}{bold_reset}")
 
 
-# HATA MESAJLARI ICIN 
-def ErrorPrinter(mesages:str):
-    print(f"{kalın}{red}[{GetTime()}]{kalın}[ERROR]: {green}{mesages}{color_reset}{kalın_reset}")
+# Fancy error stylization
+def ErrorPrinter(messages:str):
+    print(f"{bold}{red}[{GetTime()}]{bold}[ERROR]: {green}{messages}{color_reset}{bold_reset}")
 
 ###########################################################################################################
 
-# PROGRAMIN AMACINA YONELIK FONKSYINLAR 
-
-
+# The Actually Main Program Stuff (tm)
 
 
 def ConvertAnyAudio_to_wav(target_file_path:str, temp_dir_path:str=TEMP_PATH):
-    
     TARGET_FILE_FORMAT = "wav"
     
     if not os.path.exists(target_file_path) or not os.path.exists(temp_dir_path):
-        return {"success":"false", "code":"invaid path"}
+        return {"success":"false", "code":"invalid path"}
     
     target_file_extensions = target_file_path.split(".")
     target_file_extensions = target_file_extensions[len(target_file_extensions)-1]
@@ -100,10 +92,10 @@ def ConvertAnyAudio_to_wav(target_file_path:str, temp_dir_path:str=TEMP_PATH):
     supported_formats = ["MP3","OGG","FLAC","AAC","AIFF","WMA","WAV"]
     
     if target_file_extensions.upper() not in supported_formats:
-        return {"success":"false", "code":"not supported file extensions"}
+        return {"success":"false", "code":"unsupported file extension"}
 
     LoadedAudio = AudioSegment.from_file(target_file_path, format=target_file_extensions)
-    export_name = TEMP_PATH+"exported_file_"+str(random.randint(1,999))+"."+TARGET_FILE_FORMAT
+    export_name = temp_dir_path+"exported_file_"+str(random.randint(1,999))+"."+TARGET_FILE_FORMAT
     
     LoadedAudio.export(export_name, format=TARGET_FILE_FORMAT)
 
@@ -129,7 +121,7 @@ def CompareSounds(sound_1_path:str, sound_2_path:str):
     norm_sound1 = np.linalg.norm(encoded_sound1)
     norm_sound2 = np.linalg.norm(encoded_sound2)
 
-    # kosinus benzerliğini hesaplama 
+    # calculate cosine similarity
     GetSimilarity = dot_product_size / (norm_sound1 * norm_sound2)
     GetSimilarity = GetSimilarity * 100
     GetSimilarity = int(GetSimilarity)
@@ -139,62 +131,59 @@ def CompareSounds(sound_1_path:str, sound_2_path:str):
 
 ###########################################################################################################
 
-# EGER ARAC PROGRAM DEGOL MODUL OLARAK CALISOYPRSA SORUN CIKARTMAMASI ICIN
+# MAIN EXECUTION BLOCK
 
 if __name__ == "__main__":
     init()
     TitlePrinter(f"{APP_NAME} {VERSION_INFO} | {POWERED_BY}")
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--voice1", help="Target voice1 path", type=str, required=True)
-    parser.add_argument("--voice2", help="Target voice2 path", type=str, required=True)
+    parser.add_argument("--voice1", help="Path to the first voice file", type=str, required=True)
+    parser.add_argument("--voice2", help="Path to the second voice file", type=str, required=True)
 
     all_args = parser.parse_args()
     all_args = vars(all_args)
     raw_file_1 = all_args["voice1"]
     raw_file_2 = all_args["voice2"]
 
-
+    InformationPrinter("Converting files to 'wav' format...")
     raw_file_1_convert_status = ConvertAnyAudio_to_wav(target_file_path=raw_file_1)
     raw_file_2_convert_status = ConvertAnyAudio_to_wav(target_file_path=raw_file_2)
 
-    InformationPrinter("Converting files to 'vaw' format...")
     if raw_file_1_convert_status["success"] == "false" or raw_file_2_convert_status["success"] == "false":
         ErrorPrinter("File conversion failed.")
         sys.exit(1)
 
-    vaw_file_1 = raw_file_1_convert_status["path"]
-    vaw_file_2 = raw_file_2_convert_status["path"]
+    wav_file_1 = raw_file_1_convert_status["path"]
+    wav_file_2 = raw_file_2_convert_status["path"]
 
-    InformationPrinter(f"Comparing voice similarity rates...")
+    InformationPrinter("Comparing voice similarity...")
 
-    finally_status = CompareSounds(vaw_file_1, vaw_file_2)
+    final_status = CompareSounds(wav_file_1, wav_file_2)
 
-    if not finally_status["success"] == "true":
+    if not final_status["success"] == "true":
         ErrorPrinter("Audio comparison failed.")
-        os.remove(vaw_file_1)
-        os.remove(vaw_file_2)
+        os.remove(wav_file_1)
+        os.remove(wav_file_2)
         sys.exit(1)
 
-    InformationPrinter("Transaction successful...")
-    InformationPrinter("Cosine similarity is written to the screen...")
-    ses_benzerlik_oranı = finally_status["similarity"]
+    InformationPrinter("Comparison finished. Displaying results...")
+    voice_similarity_rate = final_status["similarity"]
 
-
-    if int(ses_benzerlik_oranı) < 70:
-        color = orange
-        metin = "Aynı kişi olma ihtimali düşüktür."
-    elif int(ses_benzerlik_oranı) < 60:
+    if int(voice_similarity_rate) < 60:
         color = red
-        metin = "Aynı kişi olma ihtimali çok düşüktür"
-    elif int(ses_benzerlik_oranı) >= 70:
+        text = "Low similarity - Not a likely match."
+    elif int(voice_similarity_rate) < 70:
+        color = orange
+        text = "Moderate similarity - Possible match."
+    else:
         color = green
-        metin = "Aynı kişi olma ihtimali çok yüksektir"
+        text = "High similarity - Likely match."
 
-    print(f"\n{kalın}{blue}|---------- RESULTS ----------|{color_reset}")
-    print(f"{kalın}{blue}| Similarity rate: {color} %{str(ses_benzerlik_oranı)}{color_reset}")
-    print(f"{kalın}{blue}| [lang:tr] Tespit bilgisi: {color} {str(metin)}{color_reset}")
-    print(f"{kalın}{blue}|-----------------------------|{color_reset}")
+    print(f"\n{bold}{blue}|---------- RESULTS ----------|{color_reset}")
+    print(f"{bold}{blue}| Similarity rate: {color} %{str(voice_similarity_rate)}{color_reset}")
+    print(f"{bold}{blue}| [lang:en] Detection information: {color} {str(text)}{color_reset}")
+    print(f"{bold}{blue}|-----------------------------|{color_reset}")
     
-    os.remove(vaw_file_1)
-    os.remove(vaw_file_2)
+    os.remove(wav_file_1)
+    os.remove(wav_file_2)
